708367f5f3d0684398d57df45b158217bc9f2ddb
akhil <brawls1092@gmail.com>
2026-04-28 16:56:10 +0500

    Add files via upload

---
+++ 37-sabaq.py
@@ -0,0 +1,199 @@
+# #================================MASSIVLER==============================
+
+# #=================================Listler================================
+
+
+# '''
+# ==============================MAGLIWMAT TURLERI==========================
+
+
+# int             1,23,4,5,6,1000,764646,46549846516,654654646846346
+
+# str             abdcefghijklmnopqrstuvwxyz
+
+# float           1.65,445.6
+
+# bool            True,False
+# '''
+# # a = 545
+# # b = 1.56
+# # c = True
+# # d = "Hello"
+# #-------------------------------MASSIVLERGE MISAL:------------------------
+# # A = [1.5,545,"Hello",True]
+# # print(A)
+# # Miyweler = ["Alma","Anar","Almurt","Shabdal",98]
+
+# # print(Miyweler[3])
+
+# # print(Miyweler[-1])
+
+
+
+# #===========================MASSIV FUNKCYALARI=======================
+
+# '''
+# Append
+
+# Extend
+
+# Remove
+
+# Len
+
+# Copy
+
+# Reverse
+
+# Sort
+
+# Count
+
+# Index
+
+# Clear
+
+# Pop
+
+# Insert
+
+# '''
+
+# #====================================Misallar==========================================
+
+
+# #APPEND
+# '''
+# Sanlar = [1,2,3,4,5,6]
+
+# Sanlar.append(17)
+
+# print(Sanlar)
+
+# '''
+
+
+# #Insert
+# '''
+# sanlar = [1,2,3]
+
+# sanlar.insert(1,100)
+
+# print(sanlar)
+
+# '''
+
+# #Remove
+# '''
+# sanlar = [1,2,3,2]
+
+# sanlar.remove(2)
+
+# print(sanlar)
+
+# '''
+
+# #POP
+# '''
+# sanlar = [1,2,3]
+
+# sanlar.pop(3)
+
+# print(sanlar)
+# '''
+
+# #Clear
+# '''
+# sanlar = [1,2,3,4,5,6,7]
+
+# sanlar.clear()
+
+# print(sanlar)
+# '''
+
+# #Index
+# '''
+# sanlar = [10,20,30]
+
+# print(sanlar.index(30))
+# '''
+
+
+# #Count
+# '''
+# sanlar = [1,2,2,3,2,4]
+
+# print(sanlar.count(4))
+
+# '''
+
+# #sort
+# '''
+# haripler = [1,9,4,6,7,3,5,1]
+# sanlar = ['a','i','b','c','d']
+
+# sanlar.sort()
+
+# print(sanlar)
+# '''
+
+
+# #reverse
+# '''
+# sanlar = [1,'j',9]
+
+# sanlar.reverse()
+
+# print(sanlar)
+
+# '''
+
+# #Copy
+# '''
+# a = [1,2,3,4,5]
+# b = a.copy()
+
+# print(b)
+
+# '''
+
+# #Copy ushin misal
+
+# '''
+# a = [1,2,3]
+# b = a.copy()
+
+# b.append(4)
+
+# print(a)
+
+# print(b)
+# '''
+
+# #2-Misal------------------------------------------------------------
+
+# '''
+# a = [1,2,3]
+# b = a
+
+# b.append(4)
+
+# print(a)
+# print(b)'''
+
+
+# #Extend
+# '''
+# a = [1,2]
+# b = [3,4]
+
+# a.extend(b)
+
+# print(a)
+# '''
+
+# #Len
+# '''
+# sanlar = [1,2,3,4,5,6]
+
+# print(len(sanlar))
