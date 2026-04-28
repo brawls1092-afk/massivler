2cadd81183de92a8335a588a7ec5f9dabdaa5340
akhil <brawls1092@gmail.com>
2026-04-28 16:57:52 +0500

    Add files via upload

---
+++ 31-sabaq.py
@@ -0,0 +1,203 @@
+# at = input("Atinizdi kiritin: ")
+# jas = int(input("jasinizdi kiritin: "))
+# pul = int(input("Summanizdi kiritin:"))
+# if jas <=18 and pul>=1000:
+#     print('Siz kinoga kire alasiz')
+# else:
+#     print('siz kinoga tuse almaysiz')
+# jas =20
+# testten_alingan_bali= 80
+# if jas>=18 and testten_alingan_bali>70:
+#     print("Siz student boldiniz✨")
+# else:
+#     print("Siz student bola almadiniz♦️")
+# at = input("Atinizdi kiritin: ")
+# jas = input("jasinizdi kiritin: ")
+# print('at , jas')
+
+# sanlar = [10, 20, 30, 40, 50,]
+# haripler = ['a','b','c','d','e']
+# teris_sanlar = sanlar[::-1]
+# teris_haripler = haripler[::-1]
+# qosiw = sanlar + haripler
+# print(f"{sanlar}\n{teris_sanlar}\n{teris_haripler}\n{haripler}\n{qosiw}")
+
+
+
+
+''magazin =[]
+while True:
+    print("\n1. Zat qosiw")
+    print("2. Zatti oshiriw")
+    print("3. Hamme zatti koriw")
+    print("4. Izlew ")
+    print("5. Toqtatiw")
+
+    tanlaw = input("Tanlan: ")
+
+
+    if tanlaw == '1':
+        ati = input("Qospaqshi bolgan zattin atin jazin:")
+        magazin.append(ati)
+        print("Qosildi")
+
+    elif tanlaw == '2':
+        ati = input("Oshirmekshi bolgan zattin atin jazin:")
+        if ati == magazin:
+            magazin.remove(ati)
+            print("Oshirildi")
+        else:
+            print("Tawilmadi")
+
+    elif tanlaw == '3':
+        print("Magazindegi zatlar:")
+        for i in magazin:
+            print('-',i)
+
+
+    elif tanlaw == '4':
+        ati = input("Izlew: ")
+
+        if ati in magazin:
+            print("Bar")
+        else:
+            print("Joq")
+
+    elif tanlaw == '5':
+
+        print("Kod toqtatildi.")
+        break
+    else:
+        print("Qate tanlaw")'''
+''magazin =[]
+while True:
+    print("\n1. Zat qosiw")
+    print("2. Zatti oshiriw")
+    print("3. Hamme zatti koriw")
+    print("4. Izlew ")
+    print("5. Toqtatiw")
+
+    tanlaw = input("Tanlan: ")
+
+
+    if tanlaw == '1':
+        ati = input("Qospaqshi bolgan zattin atin jazin:")
+        magazin.append(ati)
+        print("Qosildi")
+
+    elif tanlaw == '2':
+        ati = input("Oshirmekshi bolgan zattin atin jazin:")
+        if ati == magazin:
+            magazin.remove(ati)
+            print("Oshirildi")
+        else:
+            print("Tawilmadi")
+
+    elif tanlaw == '3':
+        print("Magazindegi zatlar:")
+        for i in magazin:
+            print('-',i)
+
+
+    elif tanlaw == '4':
+        ati = input("Izlew: ")
+
+        if ati in magazin:
+            print("Bar")
+        else:
+            print("Joq")
+
+    elif tanlaw == '5':
+
+        print("Kod toqtatildi.")
+        break
+    else:
+        print("Qate tanlaw")'''
+''magazin =[]
+while True:
+    print("\n1. Zat qosiw")
+    print("2. Zatti oshiriw")
+    print("3. Hamme zatti koriw")
+    print("4. Izlew ")
+    print("5. Toqtatiw")
+
+    tanlaw = input("Tanlan: ")
+
+
+    if tanlaw == '1':
+        ati = input("Qospaqshi bolgan zattin atin jazin:")
+        magazin.append(ati)
+        print("Qosildi")
+
+    elif tanlaw == '2':
+        ati = input("Oshirmekshi bolgan zattin atin jazin:")
+        if ati == magazin:
+            magazin.remove(ati)
+            print("Oshirildi")
+        else:
+            print("Tawilmadi")
+
+    elif tanlaw == '3':
+        print("Magazindegi zatlar:")
+        for i in magazin:
+            print('-',i)
+
+
+    elif tanlaw == '4':
+        ati = input("Izlew: ")
+
+        if ati in magazin:
+            print("Bar")
+        else:
+            print("Joq")
+
+    elif tanlaw == '5':
+
+        print("Kod toqtatildi.")
+        break
+    else:
+        print("Qate tanlaw")'''
+''magazin =[]
+while True:
+    print("\n1. Zat qosiw")
+    print("2. Zatti oshiriw")
+    print("3. Hamme zatti koriw")
+    print("4. Izlew ")
+    print("5. Toqtatiw")
+
+    tanlaw = input("Tanlan: ")
+
+
+    if tanlaw == '1':
+        ati = input("Qospaqshi bolgan zattin atin jazin:")
+        magazin.append(ati)
+        print("Qosildi")
+
+    elif tanlaw == '2':
+        ati = input("Oshirmekshi bolgan zattin atin jazin:")
+        if ati == magazin:
+            magazin.remove(ati)
+            print("Oshirildi")
+        else:
+            print("Tawilmadi")
+
+    elif tanlaw == '3':
+        print("Magazindegi zatlar:")
+        for i in magazin:
+            print('-',i)
+
+
+    elif tanlaw == '4':
+        ati = input("Izlew: ")
+
+        if ati in magazin:
+            print("Bar")
+        else:
+            print("Joq")
+
+    elif tanlaw == '5':
+
+        print("Kod toqtatildi.")
+        break
+    else:
+        print("Qate tanlaw")'''
